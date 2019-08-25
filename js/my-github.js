5bc11c162adf24102fdf4a89024654d63234e907
minotad66 <darwinminota@hotmail.com>
2019-08-25 10:10:03 -0500

    actualizar

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,34 @@
 // Write code here to communicate with Github
+const reposPromise = fetch("https://api.github.com/users/minotad66/repos").then(
+  response => response.json()
+);
+
+const repoWrap = document.querySelector("#repos-list");
+const quantity = document.querySelector("#repos-count");
+
+function datos_web() {
+  reposPromise
+    .then(repos => {
+      const html = repos.map(repo => {
+        return ` <div class="repo">
+        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+    </div>`;
+      });
+      return html.join("");
+    })
+    .then(htmlList => {
+      repoWrap.innerHTML = htmlList;
+    });
+}
+
+function renderLength() {
+  reposPromise
+    .then(repos => repos.length)
+    .then(length => `<p>Qty: <b> ${length} </b> </p> `)
+    .then(htmlList => {
+      quantity.innerHTML = htmlList;
+    });
+}
+
+renderLength();
+datos_web();
---
+++ js/pull-requests.js
@@ -0,0 +1,29 @@
+const pullPromise = fetch(
+  "https://api.github.com/repos/codeyourfuture/js-exercises/pulls"
+).then(response => response.json());
+
+console.log(pullPromise);
+
+const repoWrap = document.querySelector("#pull-requests-list");
+const searchPull = document.querySelector("#search");
+const btnPull = document.querySelector("#boton");
+
+function datos_pull() {
+  pullPromise.then(repos => {
+    const texto = searchPull.value;
+    console.log(texto);
+    repoWrap.innerHTML = "";
+    for (let repo of repos) {
+      console.log(repo);
+      let name = repo.user.login;
+      if (name.indexOf(texto) !== -1 && texto != "") {
+        repoWrap.innerHTML += ` <div class="repo">
+                <a href="${repo.html_url}" target="_blank">${repo.user.login}</a>
+                </div>`;
+      }
+    }
+  });
+}
+
+btnPull.addEventListener("click", datos_pull);
+searchPull.addEventListener("keyup", datos_pull);
